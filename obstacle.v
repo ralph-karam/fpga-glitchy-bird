06d840ba84f729debb843e46e53a0011f3166714
ralph-karam <ralph.karam@mail.utoronto.ca>
2025-11-17 20:39:37 -0500

    Add random module for seed generation

--- obstacle.v
+++ obstacle.v
@@ -358,6 +358,25 @@ module Up_count (Clock, Resetn, Q);
             Q <= Q + 1'b1;
 endmodule
 
+		
+module random #(parameter seedInitial = 8'd67) (reset, Clock, seed);
+    input reset;
+    input Clock;
+    output reg [7:0] seed;
+
+    wire next;
+    assign next = seed[7] ^ seed[5] ^ seed[4] ^ seed[3];
+
+    always @(posedge Clock) begin
+        if (reset) begin
+            seed <= seedInitial;
+        end else begin
+            seed <= {seed[6:0], next};
+        end
+    end
+endmodule
+
+		
 module object (Resetn, Clock, gnt, req, Y_init, Y_dim,  
                VGA_x, VGA_y, VGA_color, VGA_write, wrap);
 
