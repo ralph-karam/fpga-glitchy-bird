341cf0042b8e50718dc29e72d937a4c9d7f1b006
ralph-karam <ralph.karam@mail.utoronto.ca>
2025-11-18 17:15:11 -0500

    Update collision.v

--- collision.v
+++ collision.v
@@ -1,11 +1,30 @@
-module collision(Resetn, Clock, player_x, player_y, , collsion);
-  parameter player_H, player_W;
-  parameter GAP
+//for main
+
+assign LEDR[0] = collision;
+
+
+
+module collision(Resetn, Clock, player_x, player_y, top_h1, top_h2, top_h3, collsion);
+  parameter player_H = 32;
+  parameter player_W = 32;
+  parameter GAP = 80; 
   
   input Resetn, Clock;
   input [:] player_x, player_y;
 
   output reg collision; 
+
+  wire [:] player_top, player_bottom;
+  wire [:] player_right;
+
+  assign player_top = player_y + player_H >> 1;
+  assign player_bottom = player_y - player_H >> 1;
+  assign player_right = player_x + player_W >> 1;
+  
+  
+
+  always @(posedge Clock)
+    if (player_right == 
 
   
   
