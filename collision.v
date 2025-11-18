708bc48254d263b2cff3d4f2a3f90f7ec5bca494
ralph-karam <ralph.karam@mail.utoronto.ca>
2025-11-18 18:00:46 -0500

    Update collision.v

--- collision.v
+++ collision.v
@@ -1,35 +1,43 @@
 //for main
 
-assign LEDR[0] = collision;
+
+
+  collision pair1(CLOCK_50, player_x, player_y, top_h1, x_top1, collsion1);
+  collision pair2(CLOCK_50, player_x, player_y, top_h2, x_top3, collsion2);
+  collision pair3(CLOCK_50, player_x, player_y, top_h2, x_top3, collsion3);
+
+  assign LEDR[0] = collision;
 
 
 
-module collision(Resetn, Clock, player_x, player_y, top_h1, top_h2, top_h3, collsion);
+module collision(Clock, player_x, player_y, top_h, x_top, collsion);
   parameter player_H = 32;
   parameter player_W = 32;
+  parameter pillar_W = 50;
   parameter GAP = 80; 
+
+  parameter player_right = 116; 
+  parameter player_left == 84;
   
   input Resetn, Clock;
-  input [:] player_x, player_y;
+  input [9:0] player_x;
+  input [8:0] player_y;
+  
+  input [8:0] top_h1;
+  input [9:0] x_top;
 
   output reg collision; 
 
   wire [:] player_top, player_bottom;
-  wire [:] player_right;
 
-  assign player_top = player_y + player_H >> 1;
-  assign player_bottom = player_y - player_H >> 1;
-  assign player_right = player_x + player_W >> 1;
-  
-  
+  assign player_top = player_y - player_H >> 1;
+  assign player_bottom = player_y + player_H >> 1;
 
   always @(posedge Clock)
-    if (player_right == 
-
-  
-  
-
-
+    if (pillar_x > player_left && pillar_x < player_right)
+      if (top_h > player_top || top_h + GAP < player_bottom)
+            collision <= 1'b1;
+   else collision <= 1'b0;
   
 endmodule 
   
